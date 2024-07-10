729bbc10ec70005baa38c568b73af96185931b94
Jackson Fair <jaxsofair@gmail.com>
2024-07-10 09:05:54 -0400

    Day 1 Complete

--- 2023/problem1/p1.rs
+++ 2023/problem1/p1.rs
@@ -7,7 +7,7 @@ fn main() {
     // instantiate filename
     let filename = String::from("input.txt");
 
-    // println!("Part 1 Solution: {:?}", part1(filename.clone()));
+    println!("Part 1 Solution: {:?}", part1(filename.clone()));
     println!("Part 2 Solution: {:?}", part2(filename.clone()));
 }
 
@@ -18,7 +18,9 @@ fn part1(filename: String) -> i64 {
         for line in lines.flatten() {
             let mut l = String::from(line);
             l.retain(|c| c.is_numeric());
-            ans += format!("{}{}", l.chars().next().unwrap(), l.chars().last().unwrap()).parse::<i64>().unwrap();
+            ans += format!("{}{}", l.chars().next().unwrap(), 
+                                   l.chars().last().unwrap())
+                    .parse::<i64>().unwrap();
         }
     }
     ans
@@ -27,41 +29,38 @@ fn part1(filename: String) -> i64 {
 // part 2 of problem 1
 fn part2(filename: String) -> i64 {
     // create mapping for values
+    // the mapping has wrapping letters for cases like eighthree => 83
+    // single-letter wrapping is all that's needed due to no further overlap between
+    // words one through nine
     let mut rust_map = HashMap::new();
-    rust_map.insert("one", "1");
-    rust_map.insert("two", "2");
-    rust_map.insert("three", "3");
-    rust_map.insert("four", "4");
-    rust_map.insert("five", "5");
-    rust_map.insert("six", "6");
-    rust_map.insert("seven", "7");
-    rust_map.insert("eight", "8");
-    rust_map.insert("nine", "9");
+    rust_map.insert("one", "o1w");
+    rust_map.insert("two", "t2o");
+    rust_map.insert("three", "t3e");
+    rust_map.insert("four", "f4r");
+    rust_map.insert("five", "f5e");
+    rust_map.insert("six", "s6x");
+    rust_map.insert("seven", "s7n");
+    rust_map.insert("eight", "e8t");
+    rust_map.insert("nine", "n9e");
 
-    // sum values
     let mut ans: i64 = 0;
     if let Ok(lines) = read_lines(filename.clone()) {
         for line in lines.flatten() {
             let mut l = String::from(line);
-
-            print!("{:?}: ", l);
-            // println!("Line before: {:?}", l);
+            
+            // make numeric replacements
             for (word, num) in &rust_map {
                 l = l.replace(word, num);
             }
-            // println!("Line after: {:?}", l);
 
             l.retain(|c| c.is_numeric());
-            let num = format!("{}{}", l.chars().next().unwrap(), 
-                                      l.chars().last().unwrap())
-                        .parse::<i64>().unwrap();
-            println!("{:?}", num);
-            ans += num;
+            ans += format!("{}{}", l.chars().next().unwrap(), 
+                                   l.chars().last().unwrap())
+                    .parse::<i64>().unwrap();
         }
     }
     ans
 }
-
 
 // utility for reading each line of file iteratively
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
