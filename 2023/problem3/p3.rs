7a2b68751c200b72af49e9e7964e92e8118db1bd
Jackson Fair <jaxsofair@gmail.com>
2024-07-22 15:42:27 -0400

    Finished Day 3 Part 1

--- 2023/problem3/p3.rs
+++ 2023/problem3/p3.rs
@@ -1,30 +1,88 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
 
 fn main() {
-    // instantiate filename
-    let filename = String::from("test.txt");
-    
-    if let Ok(lines) = read_lines(filename.clone()) {
-        for line in lines.flatten() {
-            let mut l = String::from(line);
-            println!("{l}");
-        }
-    }
+    // read input file
+    let filename = String::from("input.txt");
+    let binding = read_to_string(filename.clone()).unwrap();
+    let input = binding.lines().collect::<Vec<&str>>();
 
-    
-    // println!("Part 1 Solution: {:?}", part1(filename.clone()));
+    println!("Part 1 Solution: {:?}", part1(input));
     // println!("Part 2 Solution: {:?}", part2(filename.clone()));
 }
 
-// fn part1(filename: String) -> i64 {-1}
+fn part1(input: Vec<&str>) -> i64 {
+    let mut sum = 0;
+
+    let num_rows = input.len();
+    let num_cols = input[0].len();
+
+    // iterate through each row
+    for row in 0..num_rows {
+        let mut num = "".to_string();
+        let mut surrounding: Vec<(usize, usize)> = Vec::new();
+
+        // iterate through each column
+        for col in 0..num_cols {
+            let c = input[row].chars().nth(col).unwrap();
+            if c.is_numeric() {
+                num.push(c);
+
+                // add valid surrounding indices to vector
+                surrounding.append(&mut get_surrounding(row, col, num_rows, num_cols));
+
+                // if the next character is a digit, continue
+                if col < num_cols - 1 && input[row].chars().nth(col + 1).unwrap().is_digit(10) {
+                    continue;
+                }
+                // otherwise, if all surrounding values are '.' or digits, clear
+                else if surrounding.iter().all(|(r, c)| input[*r].chars().nth(*c).unwrap() == '.' 
+                                                     || input[*r].chars().nth(*c).unwrap().is_digit(10)) {
+                    num.clear();
+                    surrounding.clear();
+                    continue;
+                }
+                // else add to sum and clear
+                else {
+                    sum += num.parse::<i64>().unwrap();
+                    num.clear();
+                    surrounding.clear();
+                    continue;
+                } 
+                
+            }
+        }
+    }
+    sum
+}
 
 // fn part2(filename: String) -> i64 {-1}
 
-// utility for reading each line of file iteratively
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn get_surrounding(row: usize, col: usize, num_rows: usize, num_cols: usize) -> Vec<(usize, usize)> {
+    let mut surrounding: Vec<(usize, usize)> = Vec::new();
+                // get grid of existent surrounding indices (including diagonals)
+                if row > 0 {
+                    surrounding.push((row - 1, col));
+                }
+                if row < num_rows - 1 {
+                    surrounding.push((row + 1, col));
+                }
+                if col > 0 {
+                    surrounding.push((row, col - 1));
+                }
+                if col < num_cols - 1 {
+                    surrounding.push((row, col + 1));
+                }
+                if row > 0 && col > 0 {
+                    surrounding.push((row - 1, col - 1));
+                }
+                if row > 0 && col < num_cols - 1 {
+                    surrounding.push((row - 1, col + 1));
+                }
+                if row < num_rows - 1 && col > 0 {
+                    surrounding.push((row + 1, col - 1));
+                }
+                if row < num_rows - 1 && col < num_cols - 1 {
+                    surrounding.push((row + 1, col + 1));
+                }
+    surrounding
 }
